35276cecd82ee19ebb859e75797e081067cf20e7
Kenny Quach <kennyquach1@gmail.com>
2023-12-03 18:34:48 +0100

    feat: use panda version for part 2

--- day-03/src/lib.rs
+++ day-03/src/lib.rs
@@ -3,9 +3,6 @@ extern crate shared;
 
 const _TEST: &'static str = include_str!("_test.txt");
 const _INPUT: &'static str = include_str!("_input.txt");
-const _MANUAL: &'static str = include_str!("_manual.txt");
-const _PANDA: &'static str = include_str!("_panda.txt");
-const _LONG: &'static str = include_str!("_long.txt");
 
 pub fn part_1() -> Solution {
     let mut previous: [bool; 140] = [false; 140];
@@ -21,7 +18,7 @@ pub fn part_1() -> Solution {
 
         for char in line.chars() {
             if char != '.' {
-                if char.is_numeric() {
+                if char.is_ascii_digit() {
                     value *= 10;
                     value += char.to_digit(10).unwrap();
 
@@ -59,15 +56,7 @@ pub fn part_1() -> Solution {
 
                     is_adjacent = false;
                 } else if value != 0 {
-                    let mut test: u32 = 1;
-                    let mut digits: usize = 0;
-                    while test <= value {
-                        test *= 10;
-                        digits += 1;
-                    }
-
-                    let start = (i - (digits - 1)).saturating_sub(2);
-                    next_remaining.push((start, i, value));
+                    next_remaining.push(create_remainder(value, i));
                 }
 
                 value = 0;
@@ -81,15 +70,7 @@ pub fn part_1() -> Solution {
                 sum += value;
             }
         } else if value != 0 {
-            let mut test: u32 = 1;
-            let mut digits: usize = 0;
-            while test <= value {
-                test *= 10;
-                digits += 1;
-            }
-
-            let start = (i - (digits - 1)).saturating_sub(2);
-            next_remaining.push((start, i, value));
+            next_remaining.push(create_remainder(value, i));
         }
 
         previous = current;
@@ -99,87 +80,98 @@ pub fn part_1() -> Solution {
     sum.into()
 }
 
+fn create_remainder(value: u32, i: usize) -> (usize, usize, u32) {
+    let mut test: u32 = 1;
+    let mut digits: usize = 0;
+    while test <= value {
+        test *= 10;
+        digits += 1;
+    }
+
+    let start = (i - (digits - 1)).saturating_sub(2);
+    (start, i, value)
+}
+
+pub const ADJ_EIGHT: [(isize, isize); 8] = [
+    (-1, 1),
+    (-1, 0),
+    (-1, -1),
+    (0, -1),
+    (0, 1),
+    (1, 1),
+    (1, 0),
+    (1, -1),
+];
+
+const HEIGHT: usize = 140;
+const WIDTH: usize = 141;
 pub fn part_2() -> Solution {
-    let data: Vec<Vec<char>> = _INPUT.lines().map(|line| line.chars().collect()).collect();
     let mut sum: u32 = 0;
-    let height = data.len();
-    let width = data[0].len();
 
-    let mut y: usize = 0;
-    for _ in 0..height {
-        let mut x: usize = 0;
+    let map: Vec<char> = _INPUT.chars().collect();
 
-        for _ in 0..width {
-            if data[y][x] == '*' {
-                if let Some(gear) = find_gear(y, x, &data, height, width) {
+    for y in 0..HEIGHT {
+        for x in 0..WIDTH - 1 {
+            if map[y * WIDTH + x] == '*' {
+                if let Some(gear) = calculate_gear_ratio(x, y, &map) {
                     sum += gear;
                 }
             }
-
-            x += 1;
         }
-
-        y += 1;
     }
 
     sum.into()
 }
 
-fn find_gear(
-    y: usize,
-    x: usize,
-    data: &Vec<Vec<char>>,
-    height: usize,
-    width: usize,
-) -> Option<u32> {
-    let mut first: Option<u32> = None;
-    let mut second: Option<u32> = None;
+fn calculate_gear_ratio(x: usize, y: usize, map: &Vec<char>) -> Option<u32> {
+    let mut count = 0;
+    let mut sum = 1;
 
-    let x_min = 0.max(x - 1);
+    for adj in ADJ_EIGHT {
+        let nx = x as isize + adj.0;
+        let ny = y as isize + adj.1;
 
-    for n_y in 0.max(y - 1)..=height.min(y + 1) {
-        for n_x in 0.max(x - 1)..=width.min(x + 1) {
-            if data[n_y][n_x].is_numeric() {
-                if n_x > x_min && data[n_y][n_x - 1].is_numeric() {
-                    continue;
-                }
+        if nx < 0 || nx >= WIDTH as isize || ny < 0 || ny >= HEIGHT as isize {
+            continue;
+        }
 
-                if first.is_none() {
-                    first = Some(get_number_around(n_y, n_x, &data));
-                } else if second.is_none() {
-                    second = Some(get_number_around(n_y, n_x, &data));
-                } else {
-                    return None;
-                }
+        let ny = ny as usize;
+        let nx = nx as usize;
+
+        if y != ny && nx > x.saturating_sub(1) && map[ny * WIDTH + nx - 1].is_ascii_digit() {
+            continue;
+        }
+
+        if map[ny * WIDTH + nx].is_ascii_digit() {
+            if count == 2 {
+                return None;
             }
+
+            sum *= expand(nx, ny, &map);
+            count += 1;
         }
     }
 
-    if let Some(a) = first {
-        if let Some(b) = second {
-            return Some(a * b);
-        }
-    }
-
-    return None;
+    (count == 2).then(|| sum)
 }
 
-fn get_number_around(y: usize, x: usize, data: &Vec<Vec<char>>) -> u32 {
+fn expand(x: usize, y: usize, map: &Vec<char>) -> u32 {
     let mut start = x;
-    while start > 0 && data[y][start - 1].is_numeric() {
-        start -= 1;
-    }
-
-    let mut end = x;
-    while end < data[0].len() - 1 && data[y][end + 1].is_numeric() {
-        end += 1;
+    loop {
+        if start > 0 && map[y * WIDTH + start - 1].is_ascii_digit() {
+            start -= 1;
+        } else {
+            break;
+        }
     }
 
     let mut value = 0;
-
-    for x in start..=end {
-        value *= 10;
-        value += data[y][x].to_digit(10).unwrap();
+    for i in start..WIDTH - 1 {
+        if let Some(digit) = map[y * WIDTH + i].to_digit(10) {
+            value = value * 10 + digit;
+        } else {
+            break;
+        }
     }
 
     value
