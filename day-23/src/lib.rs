1428bd717e8216ae3755f9c7a09c21041f94d659
Kenny Quach <kennyquach1@gmail.com>
2023-12-17 16:51:46 +0100

    chore: update templates for remaining days

--- day-23/src/lib.rs
+++ day-23/src/lib.rs
@@ -1,13 +1,44 @@
 use shared::*;
 extern crate shared;
 
-const _TEST: &'static str = include_str!("_test.txt");
 pub const _INPUT: &'static str = include_str!("_input.txt");
 
 pub fn part_1(_input: &str) -> Solution {
     Solution::None
 }
 
+#[cfg(test)]
+mod part_1_tests {
+    use crate::*;
+    use test_case::test_case;
+
+    #[test_case("", 0)]
+    fn example_input(input: &str, expected: usize) {
+        assert_eq!(part_1(input), expected.into());
+    }
+
+    #[test]
+    fn real_input() {
+        assert_eq!(part_1(_INPUT), 0usize.into());
+    }
+}
+
 pub fn part_2(_input: &str) -> Solution {
     Solution::None
+}
+
+#[cfg(test)]
+mod part_2_tests {
+    use crate::*;
+    use test_case::test_case;
+
+    #[test_case("", 0)]
+    fn example_input(input: &str, expected: usize) {
+        assert_eq!(part_1(input), expected.into());
+    }
+
+    #[test]
+    fn real_input() {
+        assert_eq!(part_1(_INPUT), 0usize.into());
+    }
 }
